d50bf344c3ffa28d33f248360244d404a9613295
cgcladera <cgcladera@gmail.com>
2014-03-19 16:54:50 +0100

    Adds bower installation

--- apidoc_template/recipes/generate.rb
+++ apidoc_template/recipes/generate.rb
@@ -1,5 +1,23 @@
 node[:deploy].each do |app_name, deploy|
 
+  script "install_bower" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<--EOH
+    npm install -g bower
+    EOH
+  end
+
+  script "generate_docs" do
+    interpreter "bash"
+    user "#{deploy[:user]}"
+    cwd "#{deploy[:deploy_to]}/current/docs-template"
+    code <<-EOH
+    bower install
+    EOH
+  end
+
   script "generate_docs" do
     interpreter "bash"
     user "root"
