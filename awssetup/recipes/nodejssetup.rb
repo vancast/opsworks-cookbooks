2bad8c2aff306b589d1e85e907b55128fa7d31c5
cgcladera <cgcladera@gmail.com>
2014-03-24 15:17:01 +0100

    Implements aws creds template rendering recipe

---
+++ awssetup/recipes/nodejssetup.rb
@@ -0,0 +1,22 @@
+node[:deploy].each do |app_name, deploy|
+  template "#{deploy[:deploy_to]}/current/config/aws.js" do
+      source "aws.js.erb"
+      mode 0660
+      group deploy[:group]
+      owner "deploy"
+      if platform?("ubuntu")
+        owner "www-data"
+      elsif platform?("amazon")
+        owner "apache"
+      end
+
+      variables(
+        :access =>     (node[:aws][:access] rescue nil),
+        :secret =>     (node[:aws][:access] rescue nil)
+      )
+
+     only_if do
+       File.directory?("#{deploy[:deploy_to]}/current/config")
+     end
+  end
+end
