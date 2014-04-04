c966ad6a02715e6179fecda00ee916dba800f365
cgcladera <cgcladera@gmail.com>
2014-04-04 10:47:38 +0200

    Fixes #2

--- apidoc/recipes/generate.rb
+++ apidoc/recipes/generate.rb
@@ -1,28 +1,28 @@
 node[:deploy].each do |app_name, deploy|
+  if File.directory?("#{deploy[:deploy_to]}/current/docs-template")
+    script "install_bower" do
+      interpreter "bash"
+      user "root"
+      cwd "#{deploy[:deploy_to]}/current"
+      code <<-EOH
+      npm install -g bower
+      EOH
+    end
 
-  script "install_bower" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    npm install -g bower
-    EOH
+    execute "bower install" do
+      cwd "#{deploy[:deploy_to]}/current/docs-template"
+      user "deploy"
+      environment ({'HOME' => '/home/deploy'})
+    end
+
+    script "generate_docs" do
+      interpreter "bash"
+      user "root"
+      cwd "#{deploy[:deploy_to]}/current"
+      code <<-EOH
+      npm install -g grunt-cli
+      grunt apidoc
+      EOH
+    end
   end
-
-  execute "bower install" do
-    cwd "#{deploy[:deploy_to]}/current/docs-template"
-    user "deploy"
-    environment ({'HOME' => '/home/deploy'})
-  end
-
-  script "generate_docs" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    npm install -g grunt-cli
-    grunt apidoc
-    EOH
-  end
-
 end
